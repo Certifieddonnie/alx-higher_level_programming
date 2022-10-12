4855337837c196624579af39a8802c9a76b87336
Certifieddonnie <mcdonaldsamure91@gmail.com>
2022-10-12 15:09:43 -0700

    ByteCode -> Python #5

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -7,7 +7,7 @@ class MagicClass:
     """defines a magicclass."""
     def __init__(self, radius=0):
         """Initialization of MagicClass"""
-        if type(radius) != int or type(radius) != float:
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
