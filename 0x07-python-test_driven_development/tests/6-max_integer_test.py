1943c0caa6ca2fcc7f8ac9963341b51a07f6a699
Certifieddonnie <mcdonaldsamure91@gmail.com>
2022-10-19 05:34:36 -0700

    Max Intrger

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -8,7 +8,7 @@ class TestMaxInteger(unittest.TestCase):
     """
     A Class to test MaxInteger function
     """
-    def max_integer(self):
+    def test_max_integer(self):
         """
         Test Function to find and return the max integer in a list of integers
         If the list is empty, the function returns None
@@ -16,4 +16,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertAlmostEqual(max_integer([6, 8, 2, 5]), 8)
         self.assertAlmostEqual(max_integer([7, 10, 7, 3]), 10)
         self.assertAlmostEqual(max_integer([2, 2, 2, 2.1]), 2.1)
-        self.assertEqual(max_integer(["McDonald",45, "Jerry"]), "McDonald")
+        self.assertEqual(max_integer(["McDonald","Glory", "Jerry"]), "McDonald")
+
+if __name__ == '__main__':
+    unittest.main()
