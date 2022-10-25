e136e58265aef1c4f7eaaa9a509bfa0997e86c18
Certifieddonnie <mcdonaldsamure91@gmail.com>
2022-10-25 21:25:56 +0100

    Pycode styling

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,5 +6,5 @@ The "1-write_file" module that writes a string to a text file(UTF8)
 
 def write_file(filename="", text=""):
     """Function that writes a string to the text file"""
-    with open(filename,"w",encoding="utf-8") as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return (f.write(text))
--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -4,6 +4,7 @@ A "3-to_json_string" module
 """
 import json
 
+
 def to_json_string(my_obj):
     """Function that returns the JSON rep of an object"""
     return (json.dumps(my_obj))
