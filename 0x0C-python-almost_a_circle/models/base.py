4b7ed265fe6521c8d3bf2050806086f41013de91
Certifieddonnie <mcdonaldsamure91@gmail.com>
2022-11-01 11:09:01 +0100

    CSV Files

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,7 @@
 The Base Module
 """
 import json
+import csv
 from io import StringIO
 
 
@@ -43,11 +44,12 @@ class Base:
         Args:
             list_objs is a list of instances who inherits of Base
         """
+        filename = cls.__name__ + ".json"
         lst = []
         if list_objs is not None:
             for i in list_objs:
                 lst.append(cls.to_dictionary(i))
-        with open("{}.json".format(cls.__name__), "w") as file:
+        with open(filename, "w") as file:
             file.write(cls.to_json_string(lst))
 
     @staticmethod
@@ -78,11 +80,55 @@ class Base:
         """returns a list of instances"""
         filename = cls.__name__ + ".json"
         lst = []
-        if (filename):
+        try:
             with open(filename, "r") as f:
                 lst = cls.from_json_string(f.read())
             for k, v in enumerate(lst):
                 lst[k] = cls.create(**lst[k])
-        else:
-            return lst
+        except Exception as e:
+            pass
+        return lst
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """serializes in CSV"""
+        filename = cls.__name__ + ".csv"
+        with open(filename, 'w', newline="") as f:
+            csvwriter = csv.writer(f)
+            if cls.__name__ == "Rectangle":
+                for obj in list_objs:
+                    csvwriter.writerow([obj.id, obj.width, obj.height, obj.x,
+                                        obj.y])
+            elif cls.__name__ == "Square":
+                for obj in list_objs:
+                    csvwriter.writerow([obj.id, obj.size, obj.x, obj.y])
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """deserializes in CSv"""
+        filename = cls.__name__ + ".csv"
+        lst = []
+        try:
+            with open(filename, 'r') as fcsv:
+                csvreader = csv.reader(fcsv)
+                for attr in csvreader:
+                    if cls.__name__ == "Rectangle":
+                        dictionary = {
+                                "id": int(attr[0]),
+                                "width": int(attr[1]),
+                                "height": int(attr[2]),
+                                "x": int(attr[3]),
+                                "y": int(attr[4])
+                                }
+                    elif cls.__name__ == "Square":
+                        dictionary = {
+                                "id": int(attr[0]),
+                                "size": int(attr[1]),
+                                "x": int(attr[2]),
+                                "y": int(attr[3])
+                                }
+                    lst_obj = cls.create(**dictionary)
+                    lst.append(lst_obj)
+        except Exception as e:
+            pass
         return lst
