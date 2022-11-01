b2e350797a12e65feb0d2dc108298ac661fc3e26
Certifieddonnie <mcdonaldsamure91@gmail.com>
2022-11-01 10:25:33 +0100

    Mandatory Done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,8 @@
 """
 The Base Module
 """
+import json
+from io import StringIO
 
 
 class Base:
@@ -21,3 +23,66 @@ class Base:
             self.id = self.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list dictionaries
+        Args:
+            list_dictionaries is a list of dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            io = StringIO()
+            json.dump(list_dictionaries, io)
+            return (io.getvalue())
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """It writes the JSON string rep of list_objs to a file
+        Args:
+            list_objs is a list of instances who inherits of Base
+        """
+        lst = []
+        if list_objs is not None:
+            for i in list_objs:
+                lst.append(cls.to_dictionary(i))
+        with open("{}.json".format(cls.__name__), "w") as file:
+            file.write(cls.to_json_string(lst))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string representation json_string
+        Args:
+            json_string is a string representation of a list of dict
+        """
+        lst = []
+        if json_string is None or len(json_string) == 0:
+            return lst
+        else:
+            io = StringIO(json_string)
+            return (json.load(io))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attribute already set"""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances"""
+        filename = cls.__name__ + ".json"
+        lst = []
+        if (filename):
+            with open(filename, "r") as f:
+                lst = cls.from_json_string(f.read())
+            for k, v in enumerate(lst):
+                lst[k] = cls.create(**lst[k])
+        else:
+            return lst
+        return lst
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -2,6 +2,7 @@
 """Unittest for base id
 """
 import unittest
+import json
 from models.base import Base
 
 class TestBase(unittest.TestCase):
@@ -42,3 +43,45 @@ class TestBase(unittest.TestCase):
         with self.assertRaises(TypeError):
             b = Base(2, 3)
 
+    def test_to_json_string(self):
+        """Tests regular to json string"""
+        Base._Base__nb_objects = 0
+        d1 = {"id": 9, "width": 5, "height": 6, "x": 7, "y": 8}
+        d2 = {"id": 2, "width": 2, "height": 3, "x": 4, "y": 0}
+        json_s = Base.to_json_string([d1, d2])
+        self.assertTrue(type(json_s) is str)
+        d = json.loads(json_s)
+        self.assertEqual(d, [d1, d2])
+
+    def test_empty_to_json_string(self):
+        """Test for passing empty list/ None"""
+        json_s = Base.to_json_string([])
+        self.assertTrue(type(json_s) is str)
+        self.assertEqual(json_s, "[]")
+
+    def test_None_to_json_String(self):
+        json_s = Base.to_json_string(None)
+        self.assertTrue(type(json_s) is str)
+        self.assertEqual(json_s, "[]")
+
+    def test_from_json_string(self):
+        """Tests regular from_json_string"""
+        json_str = '[{"id": 9, "width": 5, "height": 6, "x": 7, "y": 8}, \
+{"id": 2, "width": 2, "height": 3, "x": 4, "y": 0}]'
+        json_l = Base.from_json_string(json_str)
+        self.assertTrue(type(json_l) is list)
+        self.assertEqual(len(json_l), 2)
+        self.assertTrue(type(json_l[0]) is dict)
+        self.assertTrue(type(json_l[1]) is dict)
+        self.assertEqual(json_l[0],
+                         {"id": 9, "width": 5, "height": 6, "x": 7, "y": 8})
+        self.assertEqual(json_l[1],
+                         {"id": 2, "width": 2, "height": 3, "x": 4, "y": 0})
+
+    def test_fjs_empty(self):
+        """Tests from_json_string with an empty string"""
+        self.assertEqual([], Base.from_json_string(""))
+
+    def test_fjs_None(self):
+        """Tests from_json_string with an empty string"""
+        self.assertEqual([], Base.from_json_string(None))
