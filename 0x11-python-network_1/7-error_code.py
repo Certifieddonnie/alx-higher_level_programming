d29d62970bf7631bd71f0bae220e31c514173a38
Certifieddonnie <mcdonaldsamure91@gmail.com>
2023-01-18 15:45:31 +0100

    Code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+A Python script that takes in a URL, sends a
+request to the URL and displays the body of the
+response.
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    try:
+        r = requests.get(url)
+        print(r.text)
+    except requests.HTTPError as e:
+        print(f"Error code: {e.status_code}")
