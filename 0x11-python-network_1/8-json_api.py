13d596a36ae4bbae7761291df42b26f9c28b7e2e
Certifieddonnie <mcdonaldsamure91@gmail.com>
2023-01-18 22:20:54 +0100

    JSON API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+A Script that takes in a letter and sends a POST
+request to 'http://0.0.0.0:5000/search_user' with
+the letter as a parameter.
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if len(argv) < 2:
+        val = argv[1]
+    else:
+        val = ""
+    
+    try:
+        values = {'q': val}
+        r = requests.post(url, data=values)
+        if r.json() == {}:
+            print("No result")
+        else:
+            print(f"[{(r.json()).get('id')}] {(r.json()).get('name')}")
+    except ValueError:
+        print("Not a valid JSON")
