a63b906ee26d9dccc75394fa16b3587a8037fe91
Certifieddonnie <mcdonaldsamure91@gmail.com>
2023-01-18 14:59:20 +0100

    Post an Email #!

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+A Script that takes in a URL and an email address,
+sends a POST request to the passed URL with the
+email as a parameter, and finally displays the
+body of the response.
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    values = {'email': argv[2]}
+
+    r = requests.post(url, json=values)
+    print(r.text)
